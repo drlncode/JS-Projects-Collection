3db6e0f990139eecb76459b11f728499e46fabce
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-07-18 13:04:56 -0400

    Adding routes.

--- app/routers/homePageRouter/router.js
+++ app/routers/homePageRouter/router.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { join } from 'node:path';
 
 const homePageFile = join(process.cwd(), '/app/homePage/index.html');
+const homePageContent = join(process.cwd(), '/app/homePage/indexContent.html');
+const projectsContent = join(process.cwd(), '/app/homePage/projectsContent.html');
 const homePageDir = join(process.cwd(), '/app/homePage/');
 
 export const homePageRouter = Router();
@@ -14,13 +16,38 @@ homePageRouter.get('/', (req, res) => {
     });
 });
 
+homePageRouter.get('/home', (req, res) => {
+    res.redirect('/');
+});
+
+homePageRouter.get('/projects', (req, res) => {
+    res.redirect('/');
+});
+
 homePageRouter.get('/:folder/:file', (req, res) => {
     const { folder, file } = req.params;
     const requestFile = join(homePageDir, `/${folder}/${file}`);
-    
+
     res.sendFile(requestFile, (err) => {
         if (err) {
             res.status(404).send('File Not Found');
+        }
+    });
+});
+
+// Dynamic content.
+homePageRouter.post('/home', (req, res) => {
+    res.sendFile(homePageContent, (err) => {
+        if (err) {
+            res.status(500).send('Internal Server Error.');
+        }
+    });
+});
+
+homePageRouter.post('/projects', (req, res) => {
+    res.sendFile(projectsContent, (err) => {
+        if (err) {
+            res.status(500).send('Internal Server Error.');
         }
     });
 });
