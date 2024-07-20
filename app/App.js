d1f1ae3a0200c2e713dab5572c0709a7e50a85c5
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-07-20 11:21:07 -0400

    Server update.

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { homePageRouter } from './routers/homePageRouter/router.js';
+import { project01Router } from './routers/homePageRouter/01-router.js';
 
 export const app = express();
 
@@ -7,6 +8,9 @@ app.disable('x-powered-by');
 
 // Home router.
 app.use(homePageRouter);
+
+// Project 01 router.
+app.use(project01Router);
 
 app.use((req, res) => {
     res.status(404).send('Not Found.');
